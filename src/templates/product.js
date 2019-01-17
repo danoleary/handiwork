1d791edfe201f71512faa8b472b46bb727af6d60
Dan <danieloleary89@gmail.com>
2019-01-17 23:48:47 +0000

    use columns on product page

--- src/templates/product.js
+++ src/templates/product.js
@@ -4,36 +4,34 @@ import Img from 'gatsby-image'
 
 export default ({ pageContext: { title, price, id, image } }, location) => (
   <Layout location={location}>
-    <div className="tile is-ancestor">
-      <div className="tile is-parent">
-        <article className="tile is-child">
-          <div className="content" style={{ 'paddingTop': '100px' }}>
-            <p className="title has-text-centered is-uppercase has-text-dark-grey">
-              {title}
-            </p>
-            <p
-              className="subtitle has-text-centered is-uppercase has-text-dark-grey"
-              style={{ 'paddingTop': '50px' }}
-            >
-              £{price}
-            </p>
-            <div className="has-text-centered" style={{ 'paddingTop': '50px' }}>
-            <button className="button is-large is-outlined snipcart-add-item"
-              data-item-id={id}
-              data-item-name="Bacon"
-              data-item-price={price}
-              data-item-weight="20"
-              data-item-url="http://myapp.com/products/bacon"
-              data-item-description={title}
-            ><p className="is-size-7 has-text-black">ADD TO CART</p></button>
-            </div>
-          </div>
-        </article>
+    <div className="columns custom-columns">
+      <div className="column is-two-fifths" style={{ paddingTop: '100px' }}>
+        <p className="title has-text-centered is-uppercase has-text-dark-grey">
+          {title}
+        </p>
+        <p
+          className="subtitle has-text-centered is-uppercase has-text-dark-grey"
+          style={{ paddingTop: '50px' }}
+        >
+          £{price}
+        </p>
+        <div className="has-text-centered" style={{ paddingTop: '50px' }}>
+          <button
+            className="button is-large is-outlined snipcart-add-item"
+            data-item-id={id}
+            data-item-name="Bacon"
+            data-item-price={price}
+            data-item-weight="20"
+            data-item-url="http://myapp.com/products/bacon"
+            data-item-description={title}
+          >
+            <p className="is-size-7 has-text-black">ADD TO CART</p>
+          </button>
+        </div>
       </div>
-      <div className="tile is-parent is-7">
-        <article className="tile is-child">
-          <Img fluid={image} />
-        </article>
+
+      <div className="column">
+        <Img fluid={image} />
       </div>
     </div>
   </Layout>
